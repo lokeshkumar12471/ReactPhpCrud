23e8662b0f05bb475c423a6a8a8af248af12fc08
lokeshkumar12471 <105625414+lokeshkumar12471@users.noreply.github.com>
2023-05-07 19:22:59 +0530

    Product List Display Was Done

--- src/Component/Productlist.js
+++ src/Component/Productlist.js
@@ -3,25 +3,32 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Userlist() {
-  const [userData, setUserData] = useState([]);
+  const [product, setProduct] = useState([]);
   const [message, setMessage] = useState("");
   useEffect(() => {
-    getUserData();
+    const getProduct = () => {
+      fetch("http://localhost/reactphpcrud/api/product.php")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setProduct(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      //   const resData = await reqData.json();
+      // console.log(resData);
+      //   setUserData(resData);
+    };
+    getProduct();
   }, []);
-  const getUserData = async () => {
-    const reqData = await fetch(
-      "http://localhost/reactphpcrud/api/product.php"
-    );
-    const resData = await reqData.json();
-    // console.log(resData);
-    setUserData(resData);
-  };
+
   const handleDelete = async (std_id) => {
     const res = await axios.delete(
       "http://localhost/reactphpcrud/api/user.php/" + std_id
     );
     setMessage(res.data.success);
-    getUserData();
   };
 
   return (
@@ -44,25 +51,31 @@ function Userlist() {
                 </tr>
               </thead>
               <tbody>
-                {userData.map((uData, index) => (
+                {product.map((pData, index) => (
                   <tr key={index}>
                     <td>{index + 1}</td>
-                    <td>{uData.student_name}</td>
-                    <td>{uData.student_email}</td>
-                    <td>{uData.student_email}</td>
+                    <td>{pData.ptitle}</td>
+                    <td>{pData.pprice}</td>
+                    {/* <td>{pData.p_title}</td> */}
                     <td>
-                      {uData.student_status === "1" ? "Active" : "Inactive"}
+                      <img
+                        src={`http://localhost/reactphpcrud/productimages/${pData.pfile}`}
+                        height="100px"
+                        weight="100px"
+                        alt="product images"
+                      />
                     </td>
+                    <td>{pData.pstatus === "1" ? "Active" : "Inactive"}</td>
                     <td>
                       <Link
-                        to={"/edituser/" + uData.std_id}
+                        to={"/edituser/" + pData.std_id}
                         className="btn btn-success mx-2"
                       >
                         Edit
                       </Link>
                       <button
                         className="btn btn-danger"
-                        onClick={() => handleDelete(uData.std_id)}
+                        onClick={() => handleDelete(pData.std_id)}
                       >
                         Delete
                       </button>
